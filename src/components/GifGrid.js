44b3030fc747af9f7d671a330a2f75a5158cd26e
joelbarreraarotoma@gmail.com <joelbarreraarotoma@gmail.com>
2021-02-11 21:42:17 -0500

    First Commit

---
+++ src/components/GifGrid.js
@@ -0,0 +1,38 @@
+import React,{useState,useEffect} from 'react'
+import { getGifs } from '../helpers/getGifs';
+import { GifGriditem } from './GifGriditem';
+
+export const GifGrid = ({ category}) => {
+    //const [count, setCount] = useState(0);
+
+        const [images, setImages] = useState([]);
+
+
+    useEffect(()=>{
+        getGifs(category)
+        .then(setImages);
+    },[category])
+
+    
+
+ 
+    
+    return (
+           <>
+        <h1>{category}</h1>
+        <div className="card-grid">
+            
+                {
+                    images.map(img=>(
+                        <GifGriditem 
+                        key={img.id}
+                        {...img}
+                        />
+                    ))  
+                }
+            
+        </div>
+           
+           </>
+    )
+}
